dda488b27ba37ec917d907bdf1e75979a3e4b31c
nicolemagdz <nicolemmagdziasz@lewisu.edu>
2024-11-18 00:19:00 -0600

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>This is my React site</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
